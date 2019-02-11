1bb9d85d774fd661db71e334522717af435db060
Daniel Wivagg <dpwivagg@wpi.edu>
2019-02-11 13:09:46 -0500

    Update message type and RViz sub

--- src/plot_lidar.py
+++ src/plot_lidar.py
@@ -14,30 +14,14 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Float32MultiArray
-# TODO: Add an import for the correct message type or else we cannot use it
+from geometry_msgs.msg import Pose, PoseArray
 
-def euclid_distance(x, xi):
-    return np.sqrt(np.sum((x - xi)**2))
-
-def neighbourhood_points(X, x_centroid, distance = 5):
-    eligible_X = []
-    for x in X:
-        distance_between = euclid_distance(x, x_centroid)
-        # print('Evaluating: [%s vs %s] yield dist=%.2f' % (x, x_centroid, distance_between))
-        if distance_between <= distance:
-            eligible_X.append(x)
-    return eligible_X
-
-def gaussian_kernel(distance, bandwidth):
-    val = (1/(bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((distance / bandwidth))**2)
-    return val
 
 class Lidar:
     def __init__(self):
 
         #  This constructor sets up class variables and pubs/subs
-        self._corner_pub = rospy.Publisher('/corners', Float32MultiArray, queue_size=1)
+        self._corner_pub = rospy.Publisher('/corners', PoseArray, queue_size=1)
 
         rospy.Subscriber('/scan', LaserScan, self.sensorCallback, queue_size=1)
 
@@ -51,9 +35,18 @@ class Lidar:
         theta = np.linspace(angle_min, angle_max, ranges.size)
         # Based on inspection, using a linspace is approximately good enough to get the angles
 
-        msg = Float32MultiArray()
-        msg.data = 1
-
+        point = Pose()
+        point.position.x = 2
+        point.position.y = 2
+        point.position.z = 0
+        point.orientation.w = 0
+        point.orientation.x = 1
+        point.orientation.y = 0
+        point.orientation.z = 0
+        poses = [point]
+        msg = PoseArray()
+        msg.poses = poses
+        msg.header.frame_id = 'laser_frame'
         self._corner_pub.publish(msg)
 
 
