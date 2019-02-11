a15b4084b5d4ae2cf354cdf6593b08814fd1f58c
Daniel Wivagg <dpwivagg@wpi.edu>
2019-02-11 12:51:39 -0500

    Add necessary pub/sub and callback

--- src/plot_lidar.py
+++ src/plot_lidar.py
@@ -2,59 +2,69 @@
 
 """
 To run this node:
-First, open serial.ino and check the serial port (Should be ACM0 or something similar)
-Upload the serial.ino file to Arduino and run it
-Make sure the roscore is running and set up whatever simulation arm is being used
-Run the following commands in separate terminals:
+Make sure the roscore is running
+Run the following command:
 
-    rosrun rosserial_python serial_node.py /dev/ttyACM0 # Replace this with the actual serial port
-    rosrun RBE500 dataSmoother.py
+    rosrun lidar_slam plot_lidar.py
 
 """
 
 import rospy
-from custom_sensor_msgs.msg import ImuMin
-from geometry_msgs.msg import Twist
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float32MultiArray
 # TODO: Add an import for the correct message type or else we cannot use it
 
+def euclid_distance(x, xi):
+    return np.sqrt(np.sum((x - xi)**2))
 
-class Robot:
+def neighbourhood_points(X, x_centroid, distance = 5):
+    eligible_X = []
+    for x in X:
+        distance_between = euclid_distance(x, x_centroid)
+        # print('Evaluating: [%s vs %s] yield dist=%.2f' % (x, x_centroid, distance_between))
+        if distance_between <= distance:
+            eligible_X.append(x)
+    return eligible_X
+
+def gaussian_kernel(distance, bandwidth):
+    val = (1/(bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((distance / bandwidth))**2)
+    return val
+
+class Lidar:
     def __init__(self):
 
         #  This constructor sets up class variables and pubs/subs
+        self._corner_pub = rospy.Publisher('/corners', Float32MultiArray, queue_size=1)
 
-        #  This is the publisher. TODO: Change turtle1/cmd_vel to the topic.
-        #  TODO: Change Twist to the type of message needed to control the arm
-        self._vel_pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
-
-        # This subscribes to the Arduino/IMU. Do not change.
-        rospy.Subscriber('/arduino/imu_data', ImuMin, self.sensorCallback, queue_size=1)
+        rospy.Subscriber('/scan', LaserScan, self.sensorCallback, queue_size=1)
 
     def sensorCallback(self, data):
-        angular_x = data.angular.x
-        angular_y = data.angular.y
-        angular_z = data.angular.z
-        linear_x = data.linear.x
-        linear_y = data.linear.y
-        linear_z = data.linear.z
+        angle_min = data.angle_min
+        angle_max = data.angle_max
+        inc = data.angle_increment
+        ranges = np.asarray(data.ranges)
 
-        # TODO: The data needs to be filtered somehow (KF?)
+        # theta = np.arange(angle_min, angle_max, inc)
+        theta = np.linspace(angle_min, angle_max, ranges.size)
+        # Based on inspection, using a linspace is approximately good enough to get the angles
 
-        # TODO: Change Twist() to the correct type of message and change the variables below as needed
-        msg = Twist()
+        msg = Float32MultiArray()
+        msg.data = 1
 
-        msg.linear.x = linear_z / 5000
-        msg.linear.y = linear_x / 5000
-        msg.angular.z = angular_x / 5000
-
-        # This line does not need to change
-        self._vel_pub.publish(msg)
+        self._corner_pub.publish(msg)
 
 
 if __name__ == '__main__':
     rospy.sleep(1)
     rospy.init_node('sensor_read')
-    turtle = Robot()
+    sensor = Lidar()
 
     while not rospy.is_shutdown():
         pass
+
+
+
+
